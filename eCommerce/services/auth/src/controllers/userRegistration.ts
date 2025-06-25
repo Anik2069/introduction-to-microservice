13cda7f80d72c75523ecd308799bbff9d297e05c
Md Anik Islam <anikislam668@gmail.com>
2025-06-25 18:28:31 +0600

    cc

---
+++ eCommerce/services/auth/src/controllers/userRegistration.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from "express";
+import prisma from "@/prisma";
+import { UserCreateDTOSchema } from "@/schema";
+import bcrypt from "bcryptjs";
+const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Validate the input data against the schema
+    const parsedData = UserCreateDTOSchema.safeParse(req.body);
+    console.log(parsedData);
+    if (!parsedData.success) {
+      // If validation fails, respond with a 400 status and the validation errors
+      return res.status(400).json({
+        message: "Invalid input data",
+        errors: parsedData.error.errors,
+      });
+    }
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: parsedData.data.email,
+      },
+    });
+    if (existingUser) {
+      return res.status(409).json({
+        message: "User already exists",
+      });
+    }
+    // Create a new inventory item in the database
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, salt);
+    const newUser = await prisma.user.create({
+      data: {
+        ...parsedData.data,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        status: true,
+      },
+    });
+    // Respond with the inventory item
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
