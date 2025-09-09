50f02a07965b426d42afc7d6a08e604e57de43c0
Md Anik Islam <anikislam668@gmail.com>
2025-09-09 11:05:52 +0600

    Clear Cart ongoing

---
+++ eCommerce/services/cart/src/services/index.ts
@@ -0,0 +1,24 @@
+import redis from "@/redis";
+
+export const clearCart = async (cartId: string) => {
+  // Logic to clear the cart
+  try {
+    const data = await redis.hgetall(`cart:${cartId}`);
+    if (Object.keys(data).length === 0) {
+      return;
+    }
+
+    const items = Object.keys(data).map((key) => {
+      const { quantity, inventoryID } = JSON.parse(data[key]) as {
+        inventoryID: string;
+        quantity: number;
+      };
+      return { inventoryID, quantity, productID: key };
+    });
+
+    
+  } catch (error) {
+    console.error("Error clearing cart:", error);
+    throw new Error("Failed to clear cart");
+  }
+};
